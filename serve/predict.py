37a094827b282d3b79ca5204c5d5bc75a9d5b74b
Hugo <24921406+hjlopes@users.noreply.github.com>
2019-04-15 00:54:05 +0200

    Fixed bug in predict. Model had incorrect input, missing tokenization

--- serve/predict.py
+++ serve/predict.py
@@ -69,7 +69,8 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-    data_X, data_len  = convert_and_pad(model.word_dict, input_data)
+    words = review_to_words(input_data)
+    data_X, data_len  = convert_and_pad(model.word_dict, words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,11 +85,8 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-    output = model(data)
-    print(output)
-    if output >= 0.5:
-        result = np.array(1)
-    else:
-        result = np.array(0)
-    print("Predicted result: " + str(result))
+    with torch.no_grad():
+        output = model(data)
+    result = np.round(output.numpy())
+    
     return result
